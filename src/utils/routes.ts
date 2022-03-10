4bb5130746d088d6037ade2a3cbfc6c16fd65e97
nattapong-c <bozshijack@gmail.com>
2022-03-10 11:34:20 +0700

    remove unused code

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,10 +2,8 @@ import { Application } from "express";
 import glob from "glob";
 import path from "path";
 
-declare function require(name: string): any;
-
 const routes = (app: Application) => {
-    const files = glob.sync(path.join(__dirname, "../**/*.routes.ts"))
+    const files = glob.sync(path.join(__dirname, "../**/*.routes.ts"));
     for (let file of files) {
         app.use("/api/" + path.basename(file, ".routes.ts"), require(file))
     }
