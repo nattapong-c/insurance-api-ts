9fdb972c9a04d9c40dd4c54cc4eba6f949d8b2e5
nattapong-c <bozshijack@gmail.com>
2022-03-11 17:41:54 +0700

    add env PORT

--- src/config_env.ts
+++ src/config_env.ts
@@ -4,7 +4,7 @@ import path from 'path';
 dotenv.config({ path: path.join(__dirname, ".env") });
 
 export default {
-    EXPRESS_PORT: process.env.EXPRESS_PORT ?? 3000,
+    EXPRESS_PORT: process.env.PORT || process.env.EXPRESS_PORT,
     MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING ?? "",
     DB_NAME: process.env.DB_NAME ?? "test",
     USE_JS: process.env.USE_JS ?? null
