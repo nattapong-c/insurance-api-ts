66e55bb0dd91bcc364ec6c6186b869d3b2d85791
nattapong-c <bozshijack@gmail.com>
2022-03-11 17:34:37 +0700

    fix script build, fix routes append

--- src/config_env.ts
+++ src/config_env.ts
@@ -6,5 +6,6 @@ dotenv.config({ path: path.join(__dirname, ".env") });
 export default {
     EXPRESS_PORT: process.env.EXPRESS_PORT ?? 3000,
     MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING ?? "",
-    DB_NAME: process.env.DB_NAME ?? "test"
+    DB_NAME: process.env.DB_NAME ?? "test",
+    USE_JS: process.env.USE_JS ?? null
 };
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,11 +1,14 @@
 import { Application } from "express";
 import glob from "glob";
 import path from "path";
+import env from "../config_env";
 
 const routes = (app: Application) => {
-    const files = glob.sync(path.join(__dirname, "../**/*.routes.ts"));
+    var extension = env.USE_JS ? ".js" : ".ts";
+
+    const files = glob.sync(path.join(__dirname, "../**/*.routes" + extension));
     for (let file of files) {
-        app.use("/api/" + path.basename(file, ".routes.ts"), require(file))
+        app.use("/api/" + path.basename(file, ".routes" + extension), require(file));
     }
 }
 
